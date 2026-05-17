4069f7dbb6b082e977cb1a85c62ca0b42797b387
amujic-2 <amujic2@etf.unsa.ba>
2026-05-17 02:16:07 +0200

    Information dio stranice

---
+++ gosarajevovol3/Controllers/InformationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gosarajevol3.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Schedule(string type, string line)
+        {
+            ViewBag.Type = type;
+            ViewBag.Line = line;
+
+            return View();
+        }
+    }
+}
