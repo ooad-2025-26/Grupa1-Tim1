e49843e6ed8be45d76d04cee42448222865e561a
hhelac2 <hhelac2@etf.unsa.ba>
2026-05-17 16:59:33 +0200

    Dodan footer u layout

---
+++ gosarajevovol3/Views/Visit/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gosarajevovol3.Views.Visit
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
